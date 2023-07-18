880a5f95a18bbfe50e7b5bdb7ca18bccfa1ec783
Sean Wang <seanwang313@outlook.com>
2023-07-18 19:55:24 +0800

    Little structure changes

--- QoreLib/QoreLib/Services/DatabaseService.cs
+++ QoreLib/QoreLib/Services/DatabaseService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.Entity;
 using Microsoft.Data.Sqlite;
 using System.Data.SQLite;
 using System.Data;
 using System.IO;
 using System.Linq;
+using LiveChartsCore.Defaults;
 using Microsoft.EntityFrameworkCore;
 using QoreLib.Models;
 
@@ -13,18 +15,52 @@ namespace QoreLib.Services;
 
 public interface IDatabaseService
 {
-    ScientificDatabaseContext? SciDB { get; set; }
+    static ScientificDatabaseContext? SciDB { get; set; }
+
+    public delegate void DatabaseConnectedChangedEventHandler(bool isConnected);
+
+    event DatabaseConnectedChangedEventHandler DatabaseConnectedChanged;
     bool IsDatabaseConnected { get; set; }
+    SpectrumModel[]? SpectrumTable { get; set; }
+    TestModel[]? TestTable { get; set; }
+    int DatasetLength { get; set; }
+    string BaseFolderPath { get; set; }
     void ConnectScientificDatabase(string folderPath, string dbName);
     void CreateAndConnectScientificDatabase(string folderPath, string dbName);
     void CloseScientificDatabaseConnection();
     void AddDataToTestTable(string? name, bool isMale);
+    ObservableCollection<ObservablePoint> SelectDataToObservableCollectionById(int id);
+    int GetNextId(int currentId);
+    int GetPreviousId(int currentId);
+    int GetNextUnfilledId(int currentId);
+    int GetPreviousUnfilledId(int currentId);
+    int GetLastDataId();
+    int GetFirstDataId();
 }
 
 public class DatabaseService : IDatabaseService
 {
-    public ScientificDatabaseContext? SciDB { get; set; }
-    public bool IsDatabaseConnected { get; set; }
+    public static ScientificDatabaseContext? SciDB { get; set; }
+    public event IDatabaseService.DatabaseConnectedChangedEventHandler DatabaseConnectedChanged;
+    private bool _isDatabaseConnected;
+
+    public bool IsDatabaseConnected
+    {
+        get { return _isDatabaseConnected; }
+        set
+        {
+            if (_isDatabaseConnected != value)
+            {
+                _isDatabaseConnected = value;
+                DatabaseConnectedChanged?.Invoke(value);
+            }
+        }
+    }
+
+    public SpectrumModel[]? SpectrumTable { get; set; }
+    public TestModel[]? TestTable { get; set; }
+    public int DatasetLength { get; set; } = 0;
+    public string BaseFolderPath { get; set; } = "";
 
     public void ConnectScientificDatabase(string folderPath, string dbName)
     {
@@ -55,6 +91,7 @@ CREATE TABLE IF NOT EXISTS SpectrumTable(
     Name TEXT NOT NULL,
     IsFilled INTEGER NOT NULL,
     IsUseful INTEGER,
+    IsWrongData INTEGER DEFAULT 0,
     CreateTime TEXT NOT NULL,
     AlterTime TEXT NOT NULL,
     MarkTime TEXT
@@ -67,6 +104,11 @@ CREATE TABLE IF NOT EXISTS TestTable(
     CreateTime TEXT NOT NULL 
 );
 ");
+        SpectrumTable = SciDB.SpectrumTable.ToArray();
+        TestTable = SciDB.TestTable.ToArray();
+        DatasetLength = SpectrumTable.Length;
+        BaseFolderPath = folderPath;
+        IsDatabaseConnected = true;
     }
 
     public void CreateAndConnectScientificDatabase(string folderPath, string dbName)
@@ -105,6 +147,11 @@ CREATE TABLE IF NOT EXISTS TestTable(
     {
         SciDB?.Disconnect();
         SciDB = null;
+        SpectrumTable = null;
+        TestTable = null;
+        DatasetLength = 0;
+        BaseFolderPath = "";
+        IsDatabaseConnected = false;
     }
 
     public void AddDataToTestTable(string? name, bool isMale)
@@ -117,6 +164,162 @@ CREATE TABLE IF NOT EXISTS TestTable(
         catch (Exception e)
         {
             throw new Exception($"Error: {e.Message}");
+        }
+    }
+
+    public ObservableCollection<ObservablePoint> SelectDataToObservableCollectionById(int id)
+    {
+        if (SciDB == null)
+        {
+            throw new Exception("Database connection is not established");
+        }
+
+        var selectedData = SpectrumTable.FirstOrDefault(s => s.Id == id);
+        if (selectedData != null)
+        {
+            List<double> indexList =
+                JsonService.ReadBaseDataJson(Path.Join(BaseFolderPath, selectedData.DataFile))["Index"];
+            List<double> valueReal =
+                JsonService.ReadBaseDataJson(Path.Join(BaseFolderPath, selectedData.DataFile))["ValueReal"];
+            if (indexList.Count != valueReal.Count)
+            {
+                throw new Exception($"Inconsistent length of horizontal and vertical data with Id: {id}");
+            }
+
+            double[,] mergedArray = new double[indexList.Count, 2];
+            for (int i = 0; i < indexList.Count; i++)
+            {
+                mergedArray[i, 0] = indexList[i];
+                mergedArray[i, 1] = valueReal[i];
+            }
+
+            ObservableCollection<ObservablePoint> observableCollection = new();
+            for (int i = 0; i < mergedArray.GetLength(0); i++)
+            {
+                double x = mergedArray[i, 0];
+                double y = mergedArray[i, 1];
+                observableCollection.Add(new ObservablePoint(x, y));
+            }
+
+            return observableCollection;
+        }
+        else
+        {
+            throw new Exception($"No data found with Id: {id}");
+        }
+    }
+
+    public int GetNextId(int currentId)
+    {
+        if (SciDB == null)
+        {
+            throw new Exception("Database connection is not established");
+        }
+
+        var laterData = SpectrumTable.Where(s => s.Id > currentId).OrderBy(s => s.Id);
+        var nextData = laterData.FirstOrDefault();
+        if (nextData != null)
+        {
+            return nextData.Id;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public int GetPreviousId(int currentId)
+    {
+        if (SciDB == null)
+        {
+            throw new Exception("Database connection is not established");
+        }
+
+        var pData = SpectrumTable.Where(s => s.Id < currentId)
+            .OrderByDescending(s => s.Id);
+        var previousData = pData.LastOrDefault();
+        if (previousData != null)
+        {
+            return previousData.Id;
+        }
+        else
+        {
+            return -2;
+        }
+    }
+
+    public int GetNextUnfilledId(int currentId)
+    {
+        if (SciDB == null)
+        {
+            throw new Exception("Database connection is not established");
+        }
+
+        var unfilledData = SpectrumTable.Where(s => s.Id > currentId && s.IsFilled == false).OrderBy(s => s.Id);
+        var nextUnfilledData = unfilledData.FirstOrDefault();
+        if (nextUnfilledData != null)
+        {
+            return nextUnfilledData.Id;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public int GetPreviousUnfilledId(int currentId)
+    {
+        if (SciDB == null)
+        {
+            throw new Exception("Database connection is not established");
+        }
+
+        var unfilledData = SpectrumTable.Where(s => s.Id < currentId && s.IsFilled == false)
+            .OrderByDescending(s => s.Id);
+        var previousUnfilledData = unfilledData.LastOrDefault();
+        if (previousUnfilledData != null)
+        {
+            return previousUnfilledData.Id;
+        }
+        else
+        {
+            return -2;
+        }
+    }
+
+    public int GetLastDataId()
+    {
+        if (SciDB == null)
+        {
+            throw new Exception("Database connection is not established");
+        }
+
+        var lastData = SpectrumTable.OrderByDescending(s => s.Id).FirstOrDefault();
+        if (lastData != null)
+        {
+            return lastData.Id;
+        }
+        else
+        {
+            throw new Exception("No data in database");
+        }
+    }
+
+    public int GetFirstDataId()
+    {
+        if (SciDB == null)
+        {
+            throw new Exception("Database connection is not established");
+        }
+
+        var firstData = SpectrumTable.OrderBy(s => s.Id).FirstOrDefault();
+        if (firstData != null)
+        {
+            return firstData.Id;
+        }
+        else
+        {
+            throw new Exception("No data in database");
         }
     }
 }
---
+++ QoreLib/QoreLib/Services/JsonService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace QoreLib.Services;
+
+public class JsonService
+{
+    public static Dictionary<string, List<double>> ReadBaseDataJson(string dataFilePath)
+    {
+        string jsonString = File.ReadAllText(dataFilePath);
+        JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+        JsonElement indexArray = jsonDocument.RootElement.GetProperty("Index");
+        List<double> indexList = new();
+        if (indexArray.ValueKind == JsonValueKind.Array)
+        {
+            foreach (JsonElement element in indexArray.EnumerateArray())
+            {
+                double value = element.GetDouble();
+                indexList.Add(value);
+            }
+        }
+        JsonElement valueRealArray = jsonDocument.RootElement.GetProperty("ValueReal");
+        List<double> valueRealList = new();
+        if (valueRealArray.ValueKind == JsonValueKind.Array)
+        {
+            foreach (JsonElement element in valueRealArray.EnumerateArray())
+            {
+                double value = element.GetDouble();
+                valueRealList.Add(value);
+            }
+        }
+        JsonElement valueImagineArray = jsonDocument.RootElement.GetProperty("ValueImagine");
+        List<double> valueImagineList = new();
+        if (valueImagineArray.ValueKind == JsonValueKind.Array)
+        {
+            foreach (JsonElement element in valueImagineArray.EnumerateArray())
+            {
+                double value = element.GetDouble();
+                valueImagineList.Add(value);
+            }
+        }
+        jsonDocument.Dispose();
+        Dictionary<string, List<double>> dataToBeReturned = new()
+        {
+            { "Index", indexList },
+            { "ValueReal", valueRealList },
+            { "ValueImagine", valueImagineList }
+        };
+        return dataToBeReturned;
+    }
+}
