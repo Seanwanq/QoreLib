d2284fe2ec2cb6a9e4673cd66b8ca8c2aaf62ed7
Sean Wang <seanwang313@outlook.com>
2023-07-18 19:54:51 +0800

    Send notification in MainViewModel

---
+++ QoreLib/QoreLib/Models/NotificationModel.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Controls.Notifications;
+
+namespace QoreLib.Models;
+
+public class NotificationModel
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public NotificationType NoteType { get; set; }
+}
