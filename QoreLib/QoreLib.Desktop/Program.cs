fab16e182016ac58d310d3a682f76cde75f76edc
Sean Wang <seanwang313@outlook.com>
2023-07-19 19:33:43 +0800

    Added packages.

--- QoreLib/QoreLib.Desktop/Program.cs
+++ QoreLib/QoreLib.Desktop/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Avalonia.Svg.Skia;
 
 namespace QoreLib.Desktop;
 
@@ -14,10 +15,21 @@ class Program
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
+    // public static AppBuilder BuildAvaloniaApp()
+    //     => AppBuilder.Configure<App>()
+    //         .UsePlatformDetect()
+    //         .WithInterFont()
+    //         .LogToTrace()
+    //         .UseReactiveUI();
+
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        GC.KeepAlive(typeof(SvgImageExtension).Assembly);
+        GC.KeepAlive(typeof(Avalonia.Svg.Skia.Svg).Assembly);
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
+    }
 }
